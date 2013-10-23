17fe2b4fa9e369bf9fa1f18107bac787ba7f35da
Hector Royo <alu0100608939@ull.edu.es>
2013-10-23 16:47:54 +0100

    Segunda funcionalidad definida e implementada: Denominador

--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -9,5 +9,8 @@ describe Fraccion do
     it "Existe un valor correcto para el numerador" do
       @f1.num.should eq (1)
     end
+    it "Existe un valor correcto para el denominador" do
+      @f1.denom.should_not eq(0)
+    end
   end
 end
