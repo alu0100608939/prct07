6d3eed0ef38f61624b7ffe9999efe4b32ad88a6d
Elías Rodríguez Martín <alu0100456608@ull.edu.es>
2013-10-24 13:00:11 +0100

    Se debe sumar dos fracciones con + y dar el resultado de forma reducida (falla)

--- lib/frac_main.rb
+++ lib/frac_main.rb
@@ -1,4 +1,4 @@
-require "lib/gcd.rb"
+require "gcd.rb"
 class Fraccion
   attr_reader :num, :denom
   def initialize(x,y)
--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -1,74 +1,77 @@
-require "lib/frac_main.rb"
+require "frac_main.rb"
 
-num = 1
-denom = 2
+num1 = 1
+denom1 = 2
+num2 = -3
+denom2 = 4
 describe Fraccion do
-  before :each do
-    @f1 = Fraccion.new(num,denom)
-  end
-  describe "Racional" do
-    it "Existe un valor correcto para el numerador" do
-      @f1.num.should be_true
-    end
-    it "Existe un valor correcto para el denominador" do
-      @f1.num.should be_true
-      @f1.denom.should_not eq(0)
-    end
-    it "Los valores estan en forma reducida" do
-      @f1.denom.should eq(@f1.denom/gcd(@f1.denom,@f1.num)) 
-      @f1.num.should eq(@f1.num/gcd(@f1.denom,@f1.num))
-    end
-    it "Se debe invocar al metodo num() para obtener el numerador" do
-      @f1.num().should eq(num)
-    end
-    it "Se debe invocar al metodo num() para obtener el denominador" do
-      @f1.denom().should eq(denom)
-    end
-    it "Se debe mostar por la consola la fraccion de la forma: a/b" do
-      @f1.to_s.should eq ("#{num}/#{denom}")
-    end
-    it "Se debe mostrar por la consola la fraccion en formato flotante" do 
-      @f1.to_float.should eq (num/denom)
-    end
-    it "Se debe poder comprobar si dos fracciones son iguales con == " do
-      A = Fraccion.new(2,4)
-      (A == @f1).should be_true
-    end
-    it "Se debe calcualr el valor absoluto de una fraccion con el metodo abs" do
-      @f1.abs.should eq (@f1.num.abs/@f1.denom.abs)
-    end
-    it "Se debe calcular el reciproco de una fraccion con el metodo reciprocal" do
-      @f1.reciprocal.should eq (Fraccion.new(2,1))
-    end
-    it "Se debe calcular el opuesto de una fraccion con -" do
-      @f1.-.should eq (Fraccion.new(-1,2))
-    end
-    it "Se debe sumar dos fracciones con + y dar el resultado de forma reducida" do
-       
-    end
-    it "Se debe restar dos fracciones con - y dar el resultado de forma reducida" do
-       
-    end
-    it "Se debe multiplicar dos fracciones con * y dar el resultado de forma reducida" do
-       
-    end
-    it "Se debe dividir dos fracciones con / y dar el resultado de forma reducida" do
-       
-    end
-    it "Se debe calcular el resto dos fracciones con % y dar el resultado de forma reducida" do
-       
-    end
-    it "Se debe de poder comprobar si una fracion es menor que otra" do
-       
-    end
-    it "Se debe de poder comprobar si una fracion es mayor que otra" do
-       
-    end
-    it "Se debe de poder comprobar si una fracion es menor o igual que otra" do
-       
-    end
-    it "Se debe de poder comprobar si una fracion es mayor o igual que otra" do
-       
-    end
-  end
+   before :each do
+      @f1 = Fraccion.new(num1,denom1)
+      @f2 = Fraccion.new(num2,denom2)
+   end
+   describe "Racional" do
+      it "Existe un valor correcto para el numerador" do
+         @f1.num.should be_true
+      end
+      it "Existe un valor correcto para el denominador" do
+         @f1.num.should be_true
+         @f1.denom.should_not eq(0)
+      end
+      it "Los valores estan en forma reducida" do
+         @f1.denom.should eq(@f1.denom/gcd(@f1.denom,@f1.num)) 
+         @f1.num.should eq(@f1.num/gcd(@f1.denom,@f1.num))
+      end
+      it "Se debe invocar al metodo num() para obtener el numerador" do
+         @f1.num().should eq(num1)
+      end
+      it "Se debe invocar al metodo num() para obtener el denominador" do
+         @f1.denom().should eq(denom1)
+      end
+      it "Se debe mostar por la consola la fraccion de la forma: a/b" do
+         @f1.to_s.should eq ("#{num1}/#{denom1}")
+      end
+      it "Se debe mostrar por la consola la fraccion en formato flotante" do 
+         @f1.to_float.should eq (num1/denom1)
+      end
+      it "Se debe poder comprobar si dos fracciones son iguales con == " do
+         A = Fraccion.new(2,4)
+         (A == @f1).should be_true
+      end
+      it "Se debe calcualr el valor absoluto de una fraccion con el metodo abs" do
+         @f1.abs.should eq (@f1.num.abs/@f1.denom.abs)
+      end
+      it "Se debe calcular el reciproco de una fraccion con el metodo reciprocal" do
+         @f1.reciprocal.should eq (Fraccion.new(2,1))
+      end
+      it "Se debe calcular el opuesto de una fraccion con -" do
+         @f1.-.should eq (Fraccion.new(-1,2))
+      end
+      it "Se debe sumar dos fracciones con + y dar el resultado de forma reducida" do
+         (@f1 + @f2).should eq (Fraction.new(-1,4))
+      end
+      it "Se debe restar dos fracciones con - y dar el resultado de forma reducida" do
+         
+      end
+      it "Se debe multiplicar dos fracciones con * y dar el resultado de forma reducida" do
+         
+      end
+      it "Se debe dividir dos fracciones con / y dar el resultado de forma reducida" do
+         
+      end
+      it "Se debe calcular el resto dos fracciones con % y dar el resultado de forma reducida" do
+         
+      end
+      it "Se debe de poder comprobar si una fracion es menor que otra" do
+         
+      end
+      it "Se debe de poder comprobar si una fracion es mayor que otra" do
+         
+      end
+      it "Se debe de poder comprobar si una fracion es menor o igual que otra" do
+         
+      end
+      it "Se debe de poder comprobar si una fracion es mayor o igual que otra" do
+         
+      end
+   end
 end
