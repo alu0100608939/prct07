ef3e3f65224a5a182fb66b99281bb04f7006845f
Hector <alu0100608939@ull.edu.es>
2013-10-24 11:34:18 +0100

    Cuarta funcionalidad definida e implementada: Metodo de acceso al numerador

--- spec/frac_spec.rb
+++ spec/frac_spec.rb
@@ -7,9 +7,10 @@ describe Fraccion do
   
   describe "# Existen Elementos correctos en la fraccion" do
     it "Existe un valor correcto para el numerador" do
-      @f1.num.should eq (1)
+      @f1.num.should be_true
     end
     it "Existe un valor correcto para el denominador" do
+      @f1.num.should be_true
       @f1.denom.should_not eq(0)
     end
     it "Los valores estan en forma reducida" do
@@ -17,4 +18,10 @@ describe Fraccion do
       @f1.num.should eq(@f1.num/gcd(@f1.denom,@f1.num))
     end
   end
+  describe "# Existen metodos de acceso para los elementos" do
+    it "Se debe invocar al metodo num() para obtener el numerador" do
+      @f1.num().should eq(1)
+    end
+  end
+  
 end
