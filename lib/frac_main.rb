a2b93a88e05051a7bf2cad85b0eb278ddb768932
Elías Rodríguez Martín <alu0100456608@ull.edu.es>
2013-10-24 13:16:03 +0100

    Se debe restar dos fracciones con - y dar el resultado de forma reducida (funciona)

--- lib/frac_main.rb
+++ lib/frac_main.rb
@@ -1,40 +1,44 @@
 require "gcd.rb"
 class Fraccion
-  attr_reader :num, :denom
-  def initialize(x,y)
-    @num = x/gcd(x,y)
-    @denom = y/gcd(x,y)
-  end
-  def to_s
-    "#{@num}/#{@denom}"
-  end
-  def to_float
-    @num/@denom
-  end
-  def == (other)
-    if other.is_a? Numeric
-      return @num/@denom == other
-    end
-    @num == other.num && @denom == other.denom
-  end
-  def abs
-    @num.abs/@denom.abs
-  end
-  def reciprocal
-      Fraccion.new(@denom,@num)
-  end
-  
-  def -
-      Fraccion.new(-@num,@denom)
-  end
-  
-  def + (other)
-      #Se calculan los nuevos valores
-      num = @num*other.denom + @denom*other.num
-      denom = @denom*other.denom
-    
-      return Fraccion.new(num,denom)
-  end
+   attr_reader :num, :denom
+   def initialize(x,y)
+      @num = x/gcd(x,y)
+      @denom = y/gcd(x,y)
+   end
+   def to_s
+      "#{@num}/#{@denom}"
+   end
+   def to_float
+      @num/@denom
+   end
+   def == (other)
+      if other.is_a? Numeric
+         return @num/@denom == other
+      end
+      @num == other.num && @denom == other.denom
+   end
+   def abs
+      @num.abs/@denom.abs
+   end
+   def reciprocal
+         Fraccion.new(@denom,@num)
+   end
+   
+   def + (other)
+         #Se calculan los nuevos valores
+         num = @num*other.denom + @denom*other.num
+         denom = @denom*other.denom
+      
+         return Fraccion.new(num,denom)
+   end
+   
+   def - (other = Fraccion.new(@num*2,@denom))
+         #Se calculan los nuevos valores
+         num = @num*other.denom - @denom*other.num
+         denom = @denom*other.denom
+      
+         return Fraccion.new(num,denom)
+   end
     
 end
 
